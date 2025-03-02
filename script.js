311f4fce8709a738adb3fcde8e5e2f63ca1ba9b6
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-03-02 21:15:56 +0500

    Basic Javascript

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let colors = ["red", "blue", "green", "yellow", "purple"];
 
 // Use indexOf() to find the position of a specific color in your array.
-let indexOfColor =colors.indexOf("green");
+let indexOfColor = colors.indexOf("green");
 console.log("index of 'green':", indexOfColor);
 
 // Use concat() to combine your colors array with another array of 3 more colors.
-let newColors = colors.concat("orange","pink","brown");
+let newColors = colors.concat("orange", "pink", "brown");
 console.log(newColors);
 
 //Use push() to add a new color to the end of your array.
@@ -16,7 +16,6 @@ console.log(colors);
 let lastColor = colors.pop();
 console.log("pop:", colors);
 console.log("removed Color:", lastColor);
-
 
 //Use shift() to remove the first color from your array.
 let firstColor = colors.shift();
@@ -33,27 +32,23 @@ console.log("slice:", sliceColor);
 console.log(colors);
 
 //Use splice() to remove 2 elements from the middle of your array and replace them with 3 new colors.
-colors.splice(3, 2, "sky-blue", "dark","light-blue");
+colors.splice(3, 2, "sky-blue", "dark", "light-blue");
 console.log("splice:", colors);
-
 
 let numbers = [5, 8, 12, 3, 15, 7, 3, 11, 13, 26, 3];
 
 //Create an array of numbers and use find() to locate the first number greater than 10.
-let findNumbers = numbers.find(item => item > 10);
-    console.log("find:",findNumbers);
+let findNumbers = numbers.find((item) => item > 10);
+console.log("find:", findNumbers);
 
-    //Use filter() to create a new array containing only even numbers from your numbers array.
-    let evenNumbers = numbers.filter(item => item % 2 === 0);
-    console.log("filter:", evenNumbers);
+//Use filter() to create a new array containing only even numbers from your numbers array.
+let evenNumbers = numbers.filter((item) => item % 2 === 0);
+console.log("filter:", evenNumbers);
 
+// Loops and Iteration
+// Use a traditional for loop to log each color in your array along with its index.
+for (let i = 0; i < colors.length; i++) {
+  console.log(`Index: ${i}, Color: ${colors[i]}`);
+}
 
-    // Loops and Iteration
-    // Use a traditional for loop to log each color in your array along with its index.
-    for (let i = 0; i < colors.length; i++) {
-        console.log(`Index: ${i}, Color: ${colors[i]}`);
-        
-    }
-
-    // Use forEach() to log each color in uppercase.
-    
+// Use forEach() to log each color in uppercase.
